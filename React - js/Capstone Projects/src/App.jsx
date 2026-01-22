d39f375d2c84b7104996126d61bbc14b9618c7ce
cyberketan <panchalketan2580@gmail.com>
2026-01-22 19:53:12 +0530

    task

---
+++ React - js/Capstone Projects/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+// import './App.css'
+import AddTodo from './redux/AddTodo'
+import Todos from './redux/Todos'
+
+function App() {
+  
+
+  return (
+   <>
+   <div>Redux ToolKit</div>
+   <AddTodo/>
+   <Todos/>
+   </>
+  )
+}
+
+export default App
